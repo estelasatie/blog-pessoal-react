30581170295dacc221b3fcf6de018141348d3212
Estela Satie <estela.tatie@hotmail.com>
2024-05-09 15:35:35 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './pages/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import Cadastro from './pages/Cadastro/Cadastro';
+import Navbar from './components/navbar/navbar';
+import Home from './pages/home/Home';
+import Footer from './components/footer/footer';
+import ListaTemas from './components/temas/listaTema/listaTema';
+import FormularioTema from './components/temas/formularioTema/formularioTema';
+import DeletarTema from './components/temas/deletarTema/deletarTema';
+import ListaPostagens from './components/postagens/listaPostagem/listaPostagem';
+import FormularioPostagem from './components/postagens/formularioPostagem/formularioPostagem';
+
+import Perfil from './pages/perfil/Perfil';
+import DeletarPostagem from './components/postagens/deletarPostagem/deletarPostagem';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthProvider>
+      <ToastContainer />
+        <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/postagens/modalPostagem/modalPostagem.tsx
@@ -0,0 +1,23 @@
+
+
+import FormularioPostagem from '../formularioPostagem/formularioPostagem';
+
+import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup';
+
+import './modalPostagem.css'
+
+function ModalPostagem() {
+  return (
+    <>
+      <Popup 
+      trigger={<button className='border rounded px-4 hover:bg-white hover:text-indigo-800'>Nova postagem</button>} modal>
+        <div>
+          <FormularioPostagem />
+        </div>
+      </Popup>
+    </>
+  );
+}
+
+export default ModalPostagem;
